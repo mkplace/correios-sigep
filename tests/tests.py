1208afc33156c33b1eeb12837934c59fa38ff32a
Bruno Casado <bagcnop@gmail.com>
2018-03-05 17:42:08 -0300

    new tests

--- tests/tests.py
+++ tests/tests.py
@@ -18,15 +18,15 @@ from lxml import etree
 # def test_verifica_disponibilidade_servico():
 #     params = {
 #         'codAdministrativo': '17000190',
-#         'numeroServico': '40215',
-#         'cepOrigem': '11740000',
+#         'numeroServico': '41068',
+#         'cepOrigem': '04089001',
 #         'cepDestino': '04313000'
 
 #     }
 #     client = Client('sigep', 'n5f9t8')
 #     result = client.get_available_service(**params)
-#     assert result
-
+#     print result, 'aaaaaaaaaaaaaaaaaaa'
+#     assert  1==2
 
 def test_busca_cliente():
     params = {
@@ -34,12 +34,13 @@ def test_busca_cliente():
         'id_postcard': '0072501197'
     }
 
-    client = Client('16244974', '74558537')
+    # 74558537000108
+    client = Client('16244974', '74558537', 'sigep-production')
     result = client.get_card_services(**params)
     
-    print result, 'AAAAAAAAAAAAA'
+    # print result, 'AAAAAAAAAAAAA'
     for field in result['contratos'][0]['cartoesPostagem'][0]['servicos']:
-        print field.codigo, field.descricao, 'aaaaaaaaaaaaaaaaaaaaaaaaa'
+        print field.codigo, field.descricao
     
     assert(1==2)
 
@@ -47,8 +48,8 @@ def test_busca_cliente():
 #     params = {
 #         'recipient_type': 'C',
 #         'cnpj': '34028316000103',
-#         'service': '40215',
-#         'qtd': 2
+#         'service': '40096',
+#         'qtd': 10
 #     }
 #     # SZ27437914 BR,SZ27437914 BR
 #     client = Client('sigep', 'n5f9t8')
@@ -56,44 +57,44 @@ def test_busca_cliente():
 #     print result, 'RANGE TAG'
 #     assert(1==2)
 
-def test_fecha_plp():
-    client = Client('sigep', 'n5f9t8')
+# def test_fecha_plp():
+#     client = Client('sigep', 'n5f9t8')
     
-    tag = Tag('SZ27437914 BR')
-    tag.setDv('4')
+#     tag = Tag('SZ27437914 BR')
+#     tag.setDv('4')
 
-    plp = TagPlp.Plp('0067599079')
+#     plp = TagPlp.Plp('0067599079')
 
-    sender_obj = sender.Sender('9992157880', '72', '17000190', 'Bruno Casado',
-                  'Av. Leonardo da vinci', 146, 'Ap 31', 'Vl Guarani', 
-                  '04313000', 'Sao Paulo', 'SP', '11992590656', '', 'bagcnop@gmail.com')
+#     sender_obj = sender.Sender('9992157880', '72', '17000190', 'Bruno Casado',
+#                   'Av. Leonardo da vinci', 146, 'Ap 31', 'Vl Guarani', 
+#                   '04313000', 'Sao Paulo', 'SP', '11992590656', '', 'bagcnop@gmail.com')
 
-    recipient = Recipient('Wellington', '11992590656', '11992590656', 'wcesar@gmail.com', 'Rua samambaia', 438, 'AP 34')
+#     recipient = Recipient('Wellington', '11992590656', '11992590656', 'wcesar@gmail.com', 'Rua samambaia', 438, 'AP 34')
 
-    # objetos de postagem
-    national = National(sender_obj.bairro_remetente, sender_obj.cidade_remetente, sender_obj.uf_remetente, sender_obj.cep_remetente)
-    servicos = []
-    servico_adicional = AdditionalService(0.00)
-    servico_adicional.addCode('001')
-    servicos.append(servico_adicional)
+#     # objetos de postagem
+#     national = National(sender_obj.bairro_remetente, sender_obj.cidade_remetente, sender_obj.uf_remetente, sender_obj.cep_remetente)
+#     servicos = []
+#     servico_adicional = AdditionalService(0.00)
+#     servico_adicional.addCode('001')
+#     servicos.append(servico_adicional)
 
-    dimensao_objeto = ObjectDimension(ObjectDimension.ENVELOPE)
-    postalobject = postal_object.PostalObject(tag, '40215', '350', recipient, national, servicos,  dimensao_objeto)
+#     dimensao_objeto = ObjectDimension(ObjectDimension.ENVELOPE)
+#     postalobject = postal_object.PostalObject(tag, '40215', '350', recipient, national, servicos,  dimensao_objeto)
 
-    objeto_postal = []
-    objeto_postal.append(postalobject)
+#     objeto_postal = []
+#     objeto_postal.append(postalobject)
     
-    xml_obj = CorreiosLog(plp, sender_obj)
-    xml_obj.addPostalObject(postalobject)
+#     xml_obj = CorreiosLog(plp, sender_obj)
+#     xml_obj.addPostalObject(postalobject)
     
-    params = {
-        'id_plp': '12345',
-        'id_postcard': '0067599079',
-        'list_tags': [tag.clean(tag.tag)],
-        'xml': xml_obj.getXml()
-    }
+#     params = {
+#         'id_plp': '12345',
+#         'id_postcard': '0067599079',
+#         'list_tags': [tag.clean(tag.tag)],
+#         'xml': xml_obj.getXml()
+#     }
 
-    result = client.close_pre_post_list(**params)
+#     result = client.close_pre_post_list(**params)
     
-    print result, 'RESULTADO'
-    assert(1 == 2)
+#     print result, 'RESULTADO'
+#     assert(1 == 2)
