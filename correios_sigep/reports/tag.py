ded75f205c9b725fc326f3aaebafdda6786d1014
Bruno Casado <bagcnop@gmail.com>
2018-03-09 17:04:57 -0300

    tag  render implemented, making barcodes

--- correios_sigep/reports/tag.py
+++ correios_sigep/reports/tag.py
@@ -4,15 +4,24 @@
 from ..models.postal_object import PostalObject
 from ..models.sender import Sender
 from ..models.additional_service import AdditionalService
-import qrcode
 from elaphe import barcode
 from hubarcode.datamatrix import DataMatrixEncoder
+from candybar.CandyBarCode128 import CandyBar128
+from base64 import b64encode
+from ..reports.generator import Generator
+import qrcode
 import re
+from io import BytesIO
 
-class Tag:
-    def __init__(self, sender, postal_object, id_postcard, group = ''):
+class Tag(Generator):
+    def __init__(self, sender, postal_object, id_postcard, contract_id, group = ''):
+        super(Tag, self).__init__()
         self.str_datamatrix = ''
         self.logo = ''
+        self.contract_id = contract_id
+        self.id_postcard = id_postcard
+        self.postal_object = postal_object
+
         if not isinstance(sender, Sender):
             raise Exception(
                 'parameter sender is not valid. Must have be Sender instance')
@@ -21,7 +30,11 @@ class Tag:
             raise Exception(
                 'parameter postal_object is not valid. Must have be PostalObject instance')
 
-        self.generateDataMatrix(sender, postal_object, id_postcard)
+        self.str_datamatrix = self.generateDataMatrix(sender, postal_object, id_postcard)
+        
+        tag = self.normalizeTag(postal_object.numero_etiqueta)
+        self.img_barcode_tag = b64encode(self.generate_barcode_128(postal_object.numero_etiqueta, 300, 66, 1))
+        self.img_barcode_zipcode = b64encode(self.generate_barcode_128(postal_object.nacional.cep_destinatario, 200, 66))
 
     def mountStr(self, value):
         self.str_datamatrix += str (value)
@@ -45,9 +58,41 @@ class Tag:
         self.mountStr('-00.000000')
         self.mountStr('|')
         self.mountStr(''.ljust(30, ' '))
+
+        byte_io = BytesIO()
+        encoder = barcode('datamatrix', self.str_datamatrix, options= dict(), margin=0)
+        encoder.save(byte_io, 'png')
         
-        encoder = barcode('datamatrix', self.str_datamatrix, options= dict(), margin=10)
-        encoder.save("/tmp/test.png")
+        return b64encode(byte_io.getvalue())
+
+    def generate_barcode_128(self, value, width=300, height=80, scale=1):
+        cb128 = CandyBar128(value, width, height, scale)
+        bs = cb128.generate_barcode()
+        return bs
+        
+    def render(self):
+        params = {
+            'datamatrix': self.str_datamatrix,
+            'barcode_tag': self.img_barcode_tag,
+            'barcode_cep': self.img_barcode_zipcode,
+            'contract_id': self.contract_id,
+            'recipient_name': self.postal_object.destinatario.nome_destinatario,
+            'recipient_address': '{},{}'.format(self.postal_object.destinatario.logradouro_destinatario, self.postal_object.destinatario.numero_end_destinatario),
+            'recipient_comp': self.postal_object.destinatario.complemento_destinatario
+            'recipient_neighborhood': self.postal_object.nacional.bairro_destinatario
+            'recipient_city': self.postal_object.nacional.cidade_destinatario
+            'recipient_uf': self.postal_object.nacional.uf_destinatario
+        }
+        return super(Tag, self).render('etiqueta.html', **params)
+
+    @staticmethod
+    def normalizeTag(tag):
+        prefix_tag = tag[:2]
+        sufix_tag = tag[11:13]
+        tag = tag[2:11]
+
+        tag = '{} {} {} {} {}'.format(prefix_tag, tag[0:3], tag[3:6], tag[6:10], sufix_tag )
+        return tag
 
     @staticmethod
     def normalizeAdditionalServices(additional_service):
