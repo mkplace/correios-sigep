0c5f860554eff046cfe0682a9749aa088d194445
Bruno Casado <bagcnop@gmail.com>
2018-03-15 14:28:30 -0300

    dict normalizing fields with one _

--- correios_sigep/utils/dict.py
+++ correios_sigep/utils/dict.py
@@ -6,9 +6,9 @@ def toRecursiveDict(obj, classkey=None):
             data[k] = toRecursiveDict(v, classkey)
         return data
     elif hasattr(obj, '__dict__'):
-        data = OrderedDict([(key, toRecursiveDict(value, classkey))
+        data = OrderedDict([(key if not key.startswith('_') else key.replace('_', '', 1), toRecursiveDict(value, classkey))
                      for key, value in obj.__dict__.iteritems()
-                     if key == '_name' or not callable(value) and not key.startswith('_')])
+                     if key == '_name' or not callable(value) and not key.startswith('__')])
         
         if classkey is not None and hasattr(obj, "__class__"):
             data[classkey] = obj.__class__.__name__
