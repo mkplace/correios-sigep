e693a5bf36280baae7ad39508502977776741b86
Bruno Casado <bagcnop@gmail.com>
2018-04-17 17:58:18 -0300

    fixed packages and dependences

--- setup.py
+++ setup.py
@@ -7,9 +7,13 @@ setup(name='Correios-sigep',
       author='Bruno Casado',
       author_email='contato@brunocasado.me',
       license='MIT',
-      packages=['correios_sigep'],
+      packages=['correios_sigep', 'correios_sigep.models', 'correios_sigep.utils', 'correios_sigep.reports'],
       install_requires=[
           'lxml',
-          'zeep'
+          'zeep',
+          'elaphe',
+          'qrcode',
+          'pdfkit',
+          'candybar'
       ],
       zip_safe=False)
